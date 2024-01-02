67ef661034bc9b352970ae202b9d9f655be240cb
BarunW <wahengbam411@gmail.com>
2024-01-02 23:18:00 +0530

    setup window using glfw and glew(for screen only)

---
+++ REngine/main.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+int main()
+{
+	if (!glfwInit ()) {
+		std::cerr << "GLFW init failed..." << std::endl;
+		return -1;
+	}
+
+
+	GLFWwindow* window = glfwCreateWindow (720, 480, "OpenGL tutorial", nullptr, nullptr);
+
+	if (window == nullptr) {
+		std::cerr << "Window init failed..." << std::endl;
+		glfwTerminate ();
+		return -2;
+	}
+
+	glfwMakeContextCurrent (window);
+	//glfwSetKeyCallback (window, HandleKeyInput);
+
+	if (glewInit () != GLEW_OK) {
+		std::cerr << "Glew init failed..." << std::endl;
+		glfwDestroyWindow (window);
+		glfwTerminate ();
+		return -3;
+	}
+
+	while (!glfwWindowShouldClose (window)) {
+
+		glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers (window);
+	}
+
+
+	/*optional*/
+	glfwDestroyWindow (window);
+	glfwTerminate ();
+	/*--------*/
+
+	return 0;
+}
